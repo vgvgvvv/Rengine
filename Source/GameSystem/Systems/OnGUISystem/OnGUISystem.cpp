38b1e7506fa089f43fc92a12a8692cbbd95ec909
DongChen <352070977@qq.com>
2018-05-01 03:06:36 +0800

    添加IMGUI

---
+++ Source/GameSystem/Systems/OnGUISystem/OnGUISystem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ¶­å· on 2018/5/1.
+//
+
+#include "OnGUISystem.hpp"
---
+++ Source/GameSystem/Systems/OnGUISystem/OnGUISystem.hpp
@@ -0,0 +1,16 @@
+//
+// Created by ¶­å· on 2018/5/1.
+//
+
+#ifndef RENGINE_ONGUISYSTEM_HPP
+#define RENGINE_ONGUISYSTEM_HPP
+
+namespace ReEngine{
+    class OnGUISystem {
+
+    };
+}
+
+
+
+#endif //RENGINE_ONGUISYSTEM_HPP
